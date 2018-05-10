6731f12ccc46c3d804ed4fbfdee1fa68a8331476
Neil <neil123@gmail.com>
2018-05-09 22:40:29 -0700

    fixed errors, cleaned up directory, added abilities

--- experiment_BoW.py
+++ experiment_BoW.py
@@ -42,7 +42,7 @@ def load_bag_of_words(_file):
         lines = f.readlines()
         for line in lines:
             key, count = line.split(" ")
-            word_bag[key] = count
+            word_bag[key] = int(count)
 
     return word_bag
 
@@ -67,19 +67,32 @@ def statistically_split_word_lists(neg, pos, thresh):
         return neg_words, pos_words
 
 
-def run_bag_of_words_classification(neg, pos, negscore, posscore, max_neg_score, max_pos_score, _file):
+def run_bag_of_words_classification(neg, pos, negscore, posscore, max_neg_score, max_pos_score, _weighting, _file):
     score = 0
     with open(_file, 'r') as f:
         line = f.read()
         line = line.split(" ")
-        line = [word.translate(str.maketrans('', '', string.punctuation)) for word in line]
-        line = [word for word in line if word.isalpha()]
-        line = [word for word in line if len(word) > 1]
+        if punc:
+            line = [word.translate(str.maketrans('', '', string.punctuation)) for word in line]
+        if alpha:
+            line = [word for word in line if word.isalpha()]
+        if signif:
+            line = [word for word in line if len(word) > 1]
+        if negation:
+            line = nltk.sentiment.util.mark_negation(line)
+        if stopword:
+            line = [word for word in line if word not in set(nltk.corpus.stopwords.words('english'))]
+
         for word in line:
+            score_incrementer = 1
             if word in neg:
-                score -= 1*int(negscore[word])/max_neg_score
+                if _weighting:
+                    score_incrementer = int(negscore[word])/max_neg_score
+                score -= score_incrementer
             if word in pos:
-                score += 1*int(posscore[word])/max_pos_score
+                if _weighting:
+                    score_incrementer = int(posscore[word]) / max_pos_score
+                score += score_incrementer
     if score >= 0:
         return 1
     else:
@@ -91,16 +104,17 @@ if __name__ == "__main__":
     alpha = int(input("should only alpha words be considered"))
     stopword = int(input("should stopwords be removed"))
     negation = int(input("should we mark negations"))
+    weighting = int(input("should we use weighting"))
     max_length = int(input("define a max_length/vocab size for the BoW"))
     threshold_for_selection = float(input("Define a cutoff max ie 1.2"))
     negative_movie_review_files, positive_movie_review_files = get_movie_review_data()
-    name = './baseline_neg_word_bag.txt'
+    name = './word_bags/baseline_neg_word_bag.txt'
     # negative_bag_of_words = create_bag_of_words(name, negative_movie_review_files[:750])
     # print(len(negative_bag_of_words.keys()))
     negative_bag_of_words = load_bag_of_words(name)
     # print(len(negative_bag_of_words.keys()))
 
-    name = './baseline_pos_word_bag.txt'
+    name = './word_bags/baseline_pos_word_bag.txt'
     # positive_bag_of_words = create_bag_of_words(name, positive_movie_review_files[:750])
     # print(len(positive_bag_of_words.keys()))
     positive_bag_of_words = load_bag_of_words(name)
@@ -124,9 +138,9 @@ if __name__ == "__main__":
     negative_score = 0
     positive_score = 0
     for file in negative_movie_review_files[750:]:
-        negative_score += run_bag_of_words_classification(neg_keys, pos_keys, negative_bag_of_words, positive_bag_of_words, max_neg, max_pos, file)
+        negative_score += run_bag_of_words_classification(neg_keys, pos_keys, negative_bag_of_words, positive_bag_of_words, max_neg, max_pos, weighting, file)
     print((250-negative_score)/250)
     for file in positive_movie_review_files[750:]:
-        positive_score += run_bag_of_words_classification(neg_keys, pos_keys, negative_bag_of_words, positive_bag_of_words, max_neg, max_pos, file)
+        positive_score += run_bag_of_words_classification(neg_keys, pos_keys, negative_bag_of_words, positive_bag_of_words, max_neg, max_pos,weighting, file)
     print(positive_score/250)
     print((((250-negative_score)/250)+(positive_score/250))/2)
